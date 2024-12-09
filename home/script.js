f4d725cccda32375ad7b1a432e4329a361b98d34
FIC <fic@info.local>
2024-12-09 20:15:36 -0300

    abrir menu de hamburguer

--- home/script.js
+++ home/script.js
@@ -1,5 +1,13 @@
 function fecharCabecalhoMobile () {
+    //Pega a referência do elemento através do ID.
     let cabecalhoMobile = document.querySelector("#cabecalho-mobile");
 
     cabecalhoMobile.style.display = "none";
+}
+
+function abrirCabecalhoMobile () {
+    //Pega a referência do elemento através do ID.
+    let cabecalhoMobile = document.querySelector("#cabecalho-mobile");
+
+    cabecalhoMobile.style.display = "block";
 }
